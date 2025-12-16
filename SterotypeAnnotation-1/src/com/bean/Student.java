53a93e056b27276c5a3c238771febffcec4f125d
atharv-31 <atharvjagdale221@gmail.com>
2025-12-16 22:32:25 +0530

    SteroType

---
+++ SterotypeAnnotation-1/src/com/bean/Student.java
@@ -0,0 +1,56 @@
+package com.bean;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class Student {
+	@Value("71")
+	private int id;
+	@Value("Sachin")
+	private String name;
+	@Value("66.66")
+	private double marks;
+
+	public Student() {
+		
+	}
+
+	public Student(int id, String name, double marks) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.marks = marks;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getMarks() {
+		return marks;
+	}
+
+	public void setMarks(double marks) {
+		this.marks = marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", marks=" + marks + "]";
+	}
+
+}
